8fcc8a9aaffd9f1285b19d78835c25a2ad8c209e
huydeve <haihuy129@gmail.com>
2022-08-12 11:59:19 +0700

    feat: reuseable button components

---
+++ src/components/Button/index.js
@@ -0,0 +1,66 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import React from 'react';
+import { Link } from 'react-router-dom';
+const cx = classNames.bind(styles);
+export default function Button({
+  to,
+  href,
+  primary = false,
+  large = false,
+  medium = false,
+  small = false,
+  text = false,
+  outline = false,
+  disabled = false,
+  rounded = false,
+  color,
+  children,
+  className,
+  leftIcon,
+  rightIcon,
+  onClick,
+  ...passProps
+}) {
+  let Components = 'button';
+  const props = {
+    onClick,
+
+    ...passProps,
+  };
+
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith('on') && typeof props[key] === 'function') {
+        delete props[key];
+      }
+    });
+  }
+
+  if (to) {
+    props.to = to;
+    Components = Link;
+  } else if (href) {
+    props.href = href;
+  }
+
+  const classes = cx('wrapper', {
+    [className]: className,
+    primary,
+    outline,
+    medium,
+    text,
+    small,
+    large,
+    disabled,
+    rounded,
+    light: color === 'light',
+  });
+  return (
+    <Components className={classes} {...props}>
+      {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+      <span className={cx('title')}>{children}</span>
+      {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+    </Components>
+  );
+}
