8f2e16ac4461d7f0224cf41ea5e2d3176848b7f4
huydeve <haihuy129@gmail.com>
2022-08-15 01:15:10 +0700

    chore: remove redundant imports

--- src/layouts/components/Sidebar/Menu/MenuItem.js
+++ src/layouts/components/Sidebar/Menu/MenuItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, useMatch, useResolvedPath } from 'react-router-dom';
 import classNames from 'classnames/bind';
